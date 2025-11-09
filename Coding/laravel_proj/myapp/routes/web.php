5d3a37bff7a8969c6a62f5db0dc9c3126d963702
Moin Shadab <moinahmed5426@gmail.com>
2025-11-09 20:53:23 +0530

    done

--- Coding/laravel_proj/myapp/routes/web.php
+++ Coding/laravel_proj/myapp/routes/web.php
@@ -32,6 +32,7 @@ Route::prefix('admin')->group(function () {
     Route::get('/permissions/{userId}', [AdminController::class, 'permissions'])->name('admin.permissions');
     Route::post('/permissions/{userId}', [AdminController::class, 'assignPermission'])->name('admin.assignPermission');
 });
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
