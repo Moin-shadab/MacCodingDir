eac34af2a184acf53a48ca72cd2156f904cb4cf3
Moin Shadab <moinahmed5426@gmail.com>
2026-01-07 18:33:29 +0530

    done

--- Coding/laravel_proj/myapp/routes/web.php
+++ Coding/laravel_proj/myapp/routes/web.php
@@ -19,8 +19,6 @@ Route::get('/', function (Illuminate\Http\Request $request) {
 Auth::routes(['register' => false]);
 
 Route::prefix('admin')->group(function () {
-    
-    
     Route::get('/users', [AdminController::class, 'users'])->name('admin.users');
     Route::post('/users', [AdminController::class, 'createUser'])->name('admin.createUser');
     Route::post('/users/{id}/toggle', [AdminController::class, 'toggleActive'])->name('admin.toggleActive');
